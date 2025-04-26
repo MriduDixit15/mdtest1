ad2ddc51bf6c4d82bcc638c6634159e7b169ccb8
Mridu Dixit <mridudixit15@gmail.com>
2025-04-26 10:52:17 +0530

    updt

--- src/app/external/external.component.ts
+++ src/app/external/external.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule, isPlatformServer } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { SafeHtml, DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-external',
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
   templateUrl: './external.component.html',
   styleUrl: './external.component.scss'
